54e64a030ea488dbc475e14506ccd98033751669
Daniel Ho <1069038@lammersvilleusd.net>
2017-03-02 14:41:58 -0800

    Player 6
    Changed Player 6 from Team DHRR!
    Prepared For The Tournament 1st Round!

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -250,16 +250,21 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 6:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'loyal vengeful and continues in betraying once betrayed'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
+                size = len(history)
+                if len(opponent_history)==0: #It's the first round: collude
+                    return 'c'
+                elif history[-1]=='c' and opponent_history[-1]=='b':
+                    return 'b' # betray if they were severely punished last time
+                elif history[-1]=='b' and opponent_history[-1]=='b':
+                    return 'b' # betray if they were punished again 
+                elif history[-1]=='b' and opponent_history[-1]=='c':
+                    return 'b' # got used to betraying 
+                else:
+                    return 'c' #otherwise collude
     
 
 
