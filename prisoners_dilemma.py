3c17c982cc3edcb36607b2a25b857ec6ab9bdaea
iateacreditcard <iateacreditcard@users.noreply.github.com>
2017-03-17 14:11:55 -0700

    Third Round (#10)

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -145,14 +145,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #only betrays if they were a sucker last round.
     elif player == 2:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Demon Avenger Powerful Doggo Destroyer of Worlds'
         else:
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
+             # use history, opponent_history, score, opponent_score
+            # to compute your strategy
+            if len(opponent_history)==0: #It's the first round: betray
+                return 'b'
             elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray if they were severely punished last time
+                return 'b' # betray is they were severely punished last time
             else:
-                return 'c' #otherwise collude
+                return 'b' #otherwise collude
 
 
     
