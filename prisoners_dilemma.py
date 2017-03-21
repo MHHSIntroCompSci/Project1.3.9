baf7832f1bb5aeb331f1f0fecba94e01bb85d2e2
djonesICS <j.deanna87@yahoo.com>
2017-03-21 13:21:11 -0700

    Update prisoners_dilemma.py

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -232,7 +232,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
+                return 'b'
             elif history[-1]=='b' and opponent_history[-1]=='b':
                 return 'b' # betray is they were severely punished last time
             else:
